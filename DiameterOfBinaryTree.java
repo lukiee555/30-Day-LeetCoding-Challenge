87235c22f74a2ff008d65072a1b8f33ed386c7eb
lukiee555 <sonilokesh24@gmail.com>
2020-04-13 09:05:00 +0530

    Day - 11 Diameter Of Binary Tree

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,12 @@
+public int maxDepth(TreeNode root){
+    if(root == null)return 0;
+    int left = maxDepth(root.left);
+    int right = maxDepth(root.right);
+    max = Math.max(max, left + right);
+    return 1 + Math.max(left, right);
+}
+int max = 0;
+public int diameterOfBinaryTree(TreeNode root) {
+    maxDepth(root);
+    return max;
+}
